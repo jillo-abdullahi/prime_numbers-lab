5ff4457fe66d4a8541ff765bb84d20c48a5bce7e
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-12 00:07:14 +0300

    [Update] Added validation for function input

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,11 +1,20 @@
 def primeNumbers(test):
 	prime_numbers = []
-	for num in range(2,test):
-		if num > 1:
+	#Checking for valid inputs
+	if test < 1:
+		return []
+	if isinstance(test, list):
+		raise TypeError("test cannot be a list") 
+	if isinstance(test, dict):
+		raise TypeError("test cannot be dictionary") 
+	else:
+		for num in range(2,test):
+			#Check for prime for values greater than 1
 			for i in range(2,num):
 				if(num%i) == 0:
 					break
 			else:
 				prime_numbers.append(num)
 	return prime_numbers
+
 
