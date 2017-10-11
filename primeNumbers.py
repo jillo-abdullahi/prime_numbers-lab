09bfe10c945a9ae782c309c15111dfe7a64cf81d
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-11 23:32:28 +0300

    [update] Added prime numbers function and updated test

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,11 @@
+def primeNumbers(test):
+	prime_numbers = []
+	for num in range(2,test):
+		if num > 1:
+			for i in range(2,num):
+				if(num%i) == 0:
+					break
+			else:
+				prime_numbers.append(num)
+	return prime_numbers
 
