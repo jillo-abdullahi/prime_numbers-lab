6675c93079b7cb20f7bcdd694e84eedf11cf47fd
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-14 12:34:36 +0300

    [Update] Added doc string and slight changes to function

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,12 +1,16 @@
 def primeNumbers(test):
+	"""
+	This function takes an integer and returns a list of prime numbers from 0 to the integer 
+	"""
 	prime_numbers = []
 	#Checking for valid inputs
-	if test < 1:
-		return []
+	
 	if isinstance(test, list):
 		raise TypeError("test cannot be a list") 
-	if isinstance(test, dict):
+	elif isinstance(test, dict):
 		raise TypeError("test cannot be dictionary") 
+	elif test < 1:
+		return []
 	else:
 		for num in range(2,test):
 			#Check for prime for values greater than 1
@@ -17,4 +21,9 @@ def primeNumbers(test):
 				prime_numbers.append(num)
 	return prime_numbers
 
+
+
+if __name__ == "__main__":
+	list = primeNumbers({ 'math' : 50, 'English': 20})
+	print(list)
 
